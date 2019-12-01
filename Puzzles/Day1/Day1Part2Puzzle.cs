1849c02ae0f0e50c02285a6fcc3c4f7aca3a33cc
Schweppie <joeppetersmail@gmail.com>
2019-12-01 17:25:22 +0100

    Optimized Day1Part2 solution

--- Puzzles/Day1/Day1Part2Puzzle.cs
+++ Puzzles/Day1/Day1Part2Puzzle.cs
@@ -2,18 +2,12 @@ namespace AdventOfCode2019.Puzzles.Day1
 {
     public class Day1Part2Puzzle : Day1Puzzle
     {
-        int totalFuel = 0;
-
         public override int GetSolution()
         {
             int mass = 0;
 
             for (int i = 0; i < lines.Length; i++)
-            {
-                totalFuel = 0;
-                CalculateFuel(int.Parse(lines[i]));
-                mass += totalFuel;
-            }
+                mass += CalculateFuel(int.Parse(lines[i]));;
 
             return mass; 
         }
@@ -21,13 +15,9 @@ namespace AdventOfCode2019.Puzzles.Day1
         private int CalculateFuel(int mass)
         {
             int fuel = GetFuelRequiredForMass(mass);
-            if(fuel > 0)
-            {
-                totalFuel += fuel;
-                return CalculateFuel(fuel);
-            }
-
-            return fuel;
+            if(fuel >= 0)
+                return fuel + CalculateFuel(fuel);
+            return 0;
         }
     }
 }
