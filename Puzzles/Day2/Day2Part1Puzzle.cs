6ea71cb2b80bb21bb5d71c0c01b532ed0dfff5a6
Schweppie <joeppetersmail@gmail.com>
2019-12-02 10:28:43 +0100

    Add Day2Part1 solution

---
+++ Puzzles/Day2/Day2Part1Puzzle.cs
@@ -0,0 +1,35 @@
+namespace AdventOfCode2019.Puzzles.Day2
+{
+    public class Day2Part1Puzzle : Day2Puzzle
+    {
+        public override int GetSolution()
+        {
+            int[] programData = GetProgramData();
+
+            programData[1] = 12;
+            programData[2] = 2;
+
+            for(int i=0; i<programData.Length; i+=4)
+            {
+                int opcode = programData[i];
+                if(opcode == OPCODE_TERMINATE)
+                    break;
+
+                int xInput = programData[programData[i+1]];
+                int yInput = programData[programData[i+2]];
+                int output = -1;
+
+                if(opcode == OPCODE_ADD)
+                    output = xInput + yInput;
+                else if( opcode == OPCODE_MULTIPLY)
+                    output = xInput * yInput;
+                else
+                    return -134213412;
+
+                programData[programData[i+3]] = output;
+            }
+
+            return programData[0]; 
+        }
+    }
+}
---
+++ Puzzles/Day2/Day2Puzzle.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+
+namespace AdventOfCode2019.Puzzles.Day2
+{
+    public abstract class Day2Puzzle : PuzzleBase
+    {
+        protected const int OPCODE_TERMINATE = 99;
+        protected const int OPCODE_ADD = 1;
+        protected const int OPCODE_MULTIPLY = 2;
+
+        protected override string GetPuzzleData()
+        {
+            return "/Day2Data.txt";
+        }
+
+        protected int[] GetProgramData()
+        {
+            string[] programInput = lines[0].Split(',');
+            int[] programData = new int[programInput.Length];
+
+            for(int i=0; i< programInput.Length; i++)
+                programData[i] = int.Parse(programInput[i]);
+
+            return programData;
+        }
+   }
+}
